0adf80239ee2936478b94e7f7aac5145d948a058
angerangel <angerangel@gmail.com>
2026-05-09 08:12:17 +0200

    Update cornice.sh

--- cornice.sh
+++ cornice.sh
@@ -3,6 +3,9 @@
 #this permit to interact with mouse by xdotool
 xhost +
 
+#this is to activate the script from crontab
+eport DISPLAY=:0
+
 #this creates a list of folder containing the pictures. 
 #The example look for Immagini folder, you can change it:
 find Immagini/* -maxdepth 1 -type d > lista
