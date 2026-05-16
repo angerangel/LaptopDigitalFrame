02adb7bf0bff47580917969fcc9902ed1c673af4
angerangel <angerangel@gmail.com>
2026-05-16 08:24:40 +0200

    Update muovimouse.sh

--- muovimouse.sh
+++ muovimouse.sh
@@ -3,12 +3,17 @@
 #error output
 exec 2>> /home/max/temp.txt
 
-#this move randomly the mouse
-export DISPLAY=:0
-myx=${RANDOM:0:2}
-myy=${RANDOM:0:2}
-xdotool mousemove  $myx $myy
+#get time
+ora=`date +"%H"`
 
+#if we are between 7:00 and 22:00
+if [ $ora -gt 7  -a  $ora -gt 22 ] ; then
+   #this move randomly the mouse to turn off screensaver
+   export DISPLAY=:0
+   myx=${RANDOM:0:2}
+   myy=${RANDOM:0:2}
+   xdotool mousemove  $myx $myy
+fi
 
 
 
