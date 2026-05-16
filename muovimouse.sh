509e79b52ca828dcba1597efaa3c13173483257a
angerangel <angerangel@gmail.com>
2026-05-16 09:55:51 +0200

    Update muovimouse.sh
    -lt

--- muovimouse.sh
+++ muovimouse.sh
@@ -7,7 +7,7 @@ exec 2>> /home/max/temp.txt
 ora=`date +"%H"`
 
 #if we are between 7:00 and 22:00
-if [ $ora -gt 7  -a  $ora -gt 22 ] ; then
+if [ $ora -gt 7  -a  $ora -lt 22 ] ; then
    #this move randomly the mouse to turn off screensaver
    export DISPLAY=:0
    myx=${RANDOM:0:2}
